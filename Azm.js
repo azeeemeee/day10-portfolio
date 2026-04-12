656d9a5058350fd193e91b8c44f6c0a9b115ecd9
Azeem T <azeemtharuvara@gmail.com>
2026-04-12 21:42:04 +0530

    updated website

--- Azm.js
+++ Azm.js
@@ -282,3 +282,39 @@ if (canvas) {
 }
 
 
+const dot = document.querySelector(".cursor-dot");
+const outline = document.querySelector(".cursor-outline");
+
+let mouseX = 0, mouseY = 0;
+let outlineX = 0, outlineY = 0;
+
+/* track mouse */
+window.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+
+  dot.style.left = mouseX + "px";
+  dot.style.top = mouseY + "px";
+});
+
+/* smooth follow (INERTIA) */
+function animateCursor() {
+  outlineX += (mouseX - outlineX) * 0.20;  // lower = more lag
+  outlineY += (mouseY - outlineY) * 0.20;
+
+  outline.style.left = outlineX + "px";
+  outline.style.top = outlineY + "px";
+
+  requestAnimationFrame(animateCursor);
+}
+
+animateCursor();
+
+
+document.addEventListener("mousedown", () => {
+  document.body.style.cursor = "none";
+});
+
+document.addEventListener("mouseup", () => {
+  document.body.style.cursor = "none";
+});
